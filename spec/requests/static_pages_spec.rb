0d4d991cdbbc41b0d1d727ba0678a0ef733ffdad
Junxen <704583191@qq.com>
2014-10-10 12:52:23 +0800

    let ( :base_title )  { "Ruby on Rails Tutorial Sample App | " }

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
    describe "Static pages" do
 
+        let ( :base_title )  { "Ruby on Rails Tutorial Sample App | " }
         describe "Home page" do
 
                 it "should have the h1 'Sample App' " do
@@ -11,8 +12,7 @@ require 'rails_helper'
 
                 it "should have the title 'Home' " do
                       visit  '/static_pages/home'
-                      # expect(page).to have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
-                      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+                      expect(page).to have_title("#{base_title}Home")
                end
         end
  
@@ -25,8 +25,7 @@ require 'rails_helper'
 
                 it "should have the title 'Help' " do
                        visit  '/static_pages/help'
-                       # expect(page).to have_selector( 'title', :text => 'Ruby on Rails Tutorial Sample App | Help' )
-                       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+                        expect(page).to have_title("#{base_title}Help")
                end
        end
 
@@ -38,8 +37,19 @@ require 'rails_helper'
 
                it "should have the title 'About Us'" do
                       visit '/static_pages/about'
-                      # expect(page).to have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
-                      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+                       expect(page).to have_title("#{base_title}About Us")
                 end
        end
+
+       describe "Contact Page" do
+             it "should exist" do
+                   visit '/static_pages/contact' 
+                   expect(page).to be_truthy
+             end
+
+             it "should have title 'Contact'" do
+                  visit '/static_pages/contact'
+                   expect(page).to have_title("#{base_title}Contact")
+             end
+        end
 end
